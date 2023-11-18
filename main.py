ba72589b249ab7bfbb606042cbe528ea6f510744
Kopan <aakk123221@gmail.com>
2023-11-19 00:09:34 +0100

    Added filtering the json into a Movie class.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import src.Movie
+import src.API.filter_requests as fr
+import json
 def main():
-    pass
-
+    test_json = open(r'tests\test_json.json',encoding='utf-8')
+    movies = fr.responses_to_movies(test_json.read())
+    [print(movie.title, end="\n") for movie in movies]
+    
 if __name__ == "__main__":
     main()
