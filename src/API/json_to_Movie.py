c27ee1ad641828fd61b4e703477c7aeb7be8a728
Kopan <aakk123221@gmail.com>
2024-01-02 22:41:00 +0100

    Changed Movie structure
    Got rid of the double-constructor structure. Now filtering movie genres strings are done in json_to_Movie.py

--- src/API/json_to_Movie.py
+++ src/API/json_to_Movie.py
@@ -1,15 +1,15 @@
 # filters requests from json format to Movie class
-from ..Movie import Movie
+from src.Movie import Movie
 import json
 
 
-# transforms a singular json object into a movie object
+# Transforms a singular json object into a movie object
 def json_to_Movie(json_object: json) -> Movie:
     movie = Movie(
         title=json_object["title"],
         adult=json_object["adult"],
         poster_path=json_object["poster_path"],
-        genre_ids=json_object["genre_ids"],
+        genres=' '.join([name for (name, id) in genre_list.items() if id in json_object["genre_ids"]]),
         release_year=int(json_object["release_date"].split("-")[0]),
         vote_average=json_object["vote_average"],
         vote_count=json_object["vote_count"],
@@ -32,3 +32,25 @@ def responses_to_movies(list_of_responses: json) -> list:
     for response_list in list_of_responses:
         movies.extend(response_to_movies(response_list))
     return movies
+
+genre_list = {
+    "Action": 28,
+    "Adventure": 12,
+    "Animation": 16,
+    "Comedy": 35,
+    "Crime": 80,
+    "Documentary": 99,
+    "Drama": 18,
+    "Family": 10751,
+    "Fantasy": 14,
+    "History": 36,
+    "Horror": 27,
+    "Music": 10402,
+    "Mystery": 9648,
+    "Romance": 10749,
+    "Science Fiction": 878,
+    "TV Movie": 10770,
+    "Thriller": 53,
+    "War": 10752,
+    "Western": 37,
+}
--- src/Movie.py
+++ src/Movie.py
@@ -5,7 +5,7 @@ class Movie:
         title: str = "Undefined",
         adult: bool = False,
         poster_path: str = "",
-        genre_ids: list = [28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 10402, 9648, 10749, 878, 10770, 53, 10752, 37,],
+        genres: str = "Action Adventure Animation Comedy Crime Documentary Drama Family Fantasy History Horror Music Mystery Romance Science Fiction TV Movie Thriller War Western",
         release_year: int = 9999,
         vote_average: int = 0,
         vote_count: int = 0,
@@ -13,62 +13,17 @@ class Movie:
         self.title = title
         self.adult = adult
         self.poster_path = poster_path
-        self.genres = {name: id for (name, id) in genres.items() if id in genre_ids}
-        self.release_year = release_year
-        self.vote_average = vote_average
-        self.vote_count = vote_count
-
-    # A constructor but suited for scenario of loading Movie from the database,
-    # instead of genre_ids you get genres, which are a string representation of genre ids
-    @classmethod
-    def loaded(
-        self,
-        title: str = "Undefined",
-        adult: int = 0,
-        poster_path: str = "",
-        genres: list = [28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 10402, 9648, 10749, 878, 10770, 53, 10752, 37,],
-        release_year: int = 9999,
-        vote_average: int = 0,
-        vote_count: int = 0,
-    ):
-        self.title = title
-        self.adult = (adult == 1)
-        self.poster_path = poster_path
         self.genres = genres
         self.release_year = release_year
         self.vote_average = vote_average
         self.vote_count = vote_count
-        return self
-
-    def __str__(self) -> str:
-        return f"""Title: {self.title}
     
+    def __str__(self) -> str:
+        return (
+f"""Title: {self.title}
 Adult: {self.adult}
 Poster_path: {self.poster_path}
 Genre_ids: {self.genres}
 Release_year: {self.release_year}
 Vote_average: {self.vote_average}
-Vote_count: {self.vote_count}"""
-
-
-genres = {
-    "Action": 28,
-    "Adventure": 12,
-    "Animation": 16,
-    "Comedy": 35,
-    "Crime": 80,
-    "Documentary": 99,
-    "Drama": 18,
-    "Family": 10751,
-    "Fantasy": 14,
-    "History": 36,
-    "Horror": 27,
-    "Music": 10402,
-    "Mystery": 9648,
-    "Romance": 10749,
-    "Science Fiction": 878,
-    "TV Movie": 10770,
-    "Thriller": 53,
-    "War": 10752,
-    "Western": 37,
-}
+Vote_count: {self.vote_count}""")
