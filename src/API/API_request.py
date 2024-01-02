6560e0ceb70eb402bee211e38cef77f886ac205d
Kopan <aakk123221@gmail.com>
2024-01-02 22:42:30 +0100

    Added function for json responses loading
    New function lets you pass the key as an argument instead of loading it from a local file.

--- src/API/API_request.py
+++ src/API/API_request.py
@@ -19,20 +19,24 @@ def load_API_key_from_file() -> str:
 # gets a list of 20 movies from the page specified
 def send_API_request(category_code: int, page: int, API_KEY: str) -> str:
     # all filters that were left (year, length etc. will be filtered locally)
-
     URL = "https://advanced-movie-search.p.rapidapi.com/discover/movie"
     # get 3 pages (3 separate requests, because this API returns only 20 movies at once)
     querystring = {"with_genres": str(category_code), "page": str(page)}
-
     headers = {
         "X-RapidAPI-Key": API_KEY,
         "X-RapidAPI-Host": "advanced-movie-search.p.rapidapi.com",
     }
 
     response = requests.get(URL, headers=headers, params=querystring)
-
     return response.json()
 
+def send_n_API_requests_with_key(category_code: int, n_of_pages: int, API_KEY:str) -> list:
+    # set that holds all json responses
+    json_responses = []
+    for page_num in range(0, n_of_pages):
+        response = send_API_request(category_code, page_num + 1, API_KEY)
+        json_responses.append(response)
+    return json_responses
 
 def send_n_API_requests(category_code: int, n_of_pages: int) -> list:
     API_KEY = load_API_key_from_file()
